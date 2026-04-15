20c6c0e4f3eca604dfdcb99c5fe7213911183dc0
Aubin EBANO <ebano.aubin@gmail.com>
2026-04-15 10:38:50 +0200

    Création composant hero

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,20 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface RowsHero extends Struct.ComponentSchema {
+  collectionName: 'components_rows_heroes';
+  info: {
+    displayName: 'Hero';
+  };
+  attributes: {
+    background_image: Schema.Attribute.Media<'images' | 'files'>;
+    logo: Schema.Attribute.Media<'images' | 'files'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'rows.hero': RowsHero;
+    }
+  }
+}
