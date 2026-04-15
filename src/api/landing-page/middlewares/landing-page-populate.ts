4ccac7b438933665332afb728e08b94465af35d5
Aubin EBANO <ebano.aubin@gmail.com>
2026-04-15 16:14:24 +0200

    Création d'un middleware pour gérer la populate de la requête landing page

---
+++ src/api/landing-page/middlewares/landing-page-populate.ts
@@ -0,0 +1,30 @@
+/**
+ * `landingPagePopulate` middleware
+ */
+
+import type { Core } from '@strapi/strapi';
+
+const populate = {
+        hero_banner: {
+                populate: {
+                        logo: {
+                                fields: ["url"],
+                        },
+                        background_image: {
+                                fields: ["url"],
+                        },
+                },
+        },
+};
+
+export default (config, { strapi }: { strapi: Core.Strapi }) => {
+        // Add your own logic here.
+        return async (ctx, next) => {
+
+                ctx.query.populate = populate;
+
+                strapi.log.info('In landingPagePopulate middleware.');
+
+                await next();
+        };
+};
